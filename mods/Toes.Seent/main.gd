3ccb0d4c73a3bf84f0e50646a6a55274e3988161
binury <1146921+binury@users.noreply.github.com>
2025-12-16 11:52:12 -0500

    Fix: player stuck after power-up

--- mods/Toes.Seent/main.gd
+++ mods/Toes.Seent/main.gd
@@ -1,3 +1,23 @@
+# Copyright (c) 2025 binury
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 extends Node
 
 const BASIC_BADGE_COLOR := "#e69d00"
@@ -132,6 +152,8 @@ func _process(delta: float) -> void:
 				# Just in case...
 				yield(get_tree().create_timer(2.0), "timeout")
 				Players.local_player._exit_animation()
+				# Surprisingly, exit_animation does not unlock so...
+				Players.local_player.locked = false
 
 				Chat.write("\n[center][i]%s[/i][/center]" % Players.get_username(player))
 				Chat.write("[center][rainbow][i][b]PAL PROXIMITY POWER-UP![/b][/i][/rainbow][/center]")
@@ -364,6 +386,7 @@ func init_player(player: Actor) -> void:
 
 
 func get_times_seen_badge(id: String, rich: bool = true) -> String:
+	if not Players.local_player: return ""
 	if str(Players.local_player.owner_id) == id or Players.check(id) == false or Players.is_player_ignored(id):
 		return ""
 	var times_badge := ""
